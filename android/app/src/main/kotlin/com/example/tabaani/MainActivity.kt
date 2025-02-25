e09163bd51d395d1f6a57b4d3b4272082083bd9d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2025-02-25 15:10:49 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tabaani/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tabaani
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/utilisateur_model.dart
@@ -0,0 +1,36 @@
+class UtilisateurModel {
+  final String? id;
+  final String nom;
+  final String prenom;
+  final String email;
+  final String mdp;
+  final String role;
+  UtilisateurModel(
+      {this.id,
+      required this.nom,
+      required this.prenom,
+      required this.email,
+      required this.mdp,
+      required this.role});
+  // factory
+  factory UtilisateurModel.fromJson(Map<String, dynamic> json) {
+    return UtilisateurModel(
+        id: json['id'],
+        nom: json['nom'],
+        prenom: json['prenom'],
+        email: json['email'],
+        mdp: json['mdp'],
+        role: json['role']);
+  }
+  // Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'nom': nom,
+      'prenom': prenom,
+      'email': email,
+      'mdp': mdp,
+      'role': role
+    };
+  }
+}
